76aeecfd4b25c1aeba72b863b2021874ce4ba681
Sam Shiles <shilessam@hotmail.com>
2018-10-28 14:51:23 -0400

    Revert "Start to add swagger"
    This reverts commit f531c398a2869f5a8a317034f52b91ec400e793c.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const restify = require("restify");
 const restify_router_1 = require("restify-router");
-const Swagger = require("swagger-restify-mw");
 const loggerFactory_1 = require("./util/loggerFactory");
 const routes_1 = require("./start/routes");
 const logger = new loggerFactory_1.LoggerFactory().create();
@@ -17,18 +16,5 @@ router.add("/api", baseRoutes);
 router.applyRoutes(server);
 server.listen(8080, () => {
     logger.info(`Listen on port:${server.url}`);
-});
-var config = {
-    appRoot: __dirname
-};
-Swagger.create(config, function (err, swaggerRestify) {
-    if (err) {
-        throw err;
-    }
-    swaggerRestify.register(server);
-    var port = process.env.PORT || 10010;
-    if (swaggerRestify.runner.swagger.paths['/hello']) {
-        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-    }
 });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import * as restify from 'restify';
 import {Router} from 'restify-router';
-import * as Swagger from 'swagger-restify-mw'
 import {LoggerFactory} from './util/loggerFactory';
 import {RouteConfig} from './start/routes'
 
@@ -18,23 +17,4 @@ router.applyRoutes(server);
 
 server.listen(8080, () => {
     logger.info(`Listen on port:${server.url}`);
-});
-
-
-var config = {
-    appRoot: __dirname // required config
-  };
-
-
-
-Swagger.create(config, function(err, swaggerRestify) {
-if (err) { throw err; }
-
-swaggerRestify.register(server);
-
-var port = process.env.PORT || 10010;
-
-if (swaggerRestify.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-}
 });
